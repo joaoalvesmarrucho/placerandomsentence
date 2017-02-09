a09674d46e78ca563cadbaeb1d85c03e82188b56
Joao Marrucho <joaoalvesmarrucho@gmail.com>
2017-02-09 16:38:03 +0000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,19 @@
+var sentences = [
+  "Saab. ",
+  "Volvo. ",
+  "BMW. ",
+  "Ford. "
+];
+var element;
+element = document.getElementById(container);
+
+console.log(container);
+
+function randomise() {
+  var randomSentence = sentences[Math.floor(Math.random() * 4)];
+  console.log(randomSentence);
+   $(container).append(randomSentence);
+  return randomSentence;
+}
+setInterval(function(){randomise(); }, 1000);
+// randomise();

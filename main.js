8571f1271a565da40028f8addeabc55b3188bbdc
GCD Crosslab <j.demaat@arts.ac.uk>
2017-02-09 22:43:40 +0000

    Update main.js
    adding notes

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+//to quickly add sentences in the right format as below use Find and Replay in for example indesign. Search for . replace with . ","
+
 var sentences = [
   "As armas e os barões assinalados. ",
   "Que da ocidental praia Lusitana. ",
@@ -9,4 +11,5 @@ function randomise() {
   var randomSentence = sentences[Math.floor(Math.random() * 4)];
    $(container).append(randomSentence);
 }
+// set interval time in miliseconds
 setInterval(function(){randomise(); }, 2000);
